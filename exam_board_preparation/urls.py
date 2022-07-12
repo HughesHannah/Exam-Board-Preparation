26852d8cde8873b7d561276d552785d096fd116e
Hannah Hughes <hrh96@hotmail.co.uk>
2022-07-12 15:44:25 +0100

    favicon added

--- exam_board_preparation/urls.py
+++ exam_board_preparation/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),
+    ),
     path('', include('exam_board_preparation_app.urls')),
     path('', include('frontend.urls'))
 ]
