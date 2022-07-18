29d044c5bb02f00f73d9eb029a9f4f5122005699
Hannah Hughes <hrh96@hotmail.co.uk>
2022-07-18 14:21:57 +0100

    login page scss

--- frontend/src/pages/login/Login.js
+++ frontend/src/pages/login/Login.js
@@ -6,11 +6,25 @@ import AuthContext from "../../context/AuthContext.js"
 const Login = () => {
   let {loginUser} = useContext(AuthContext)
   return (<div className="login">
-    <form className="form" onSubmit={loginUser}>
-      <input type="text" name="username" placeholder="Enter Username"/>
-      <input type="password" name="password" placeholder="Enter Password"/>
-      <input type="submit" />
-    </form>
+    <div className="mainContainer">
+      <div className="logoTitle">Exam Board Preparation Page</div>
+      <div className="loginBox">
+        <form className="form" onSubmit={loginUser}>
+          <div className="inputDiv">
+            <input type="text" name="username" placeholder="Enter Username" className="inputBox"/>
+          </div>
+          <div className="inputDiv">
+            <input type="password" name="password" placeholder="Enter Password" className="inputBox"/>
+          </div>
+          <div className="inputDiv">
+            <input type="submit" className="button"/>
+          </div>
+        </form>
+        <p className="forgotP">Forgot Password?</p>
+      </div>
+    </div>
+    
+    
   </div>);
 };
 
