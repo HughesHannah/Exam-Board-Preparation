81ddce5ef5abd836951c99f894941a02fd075c4e
Hannah Hughes <hrh96@hotmail.co.uk>
2022-07-21 13:07:07 +0100

    updated models

--- exam_board_preparation_app/admin.py
+++ exam_board_preparation_app/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from exam_board_preparation_app.models import Student, Course, Cohort, Exam, Coursework, Departments, ClassHead
+from exam_board_preparation_app.models import Student, Course, Exam, Assignment, Departments, ClassHead
 
 
 # Register your models here.
 admin.site.register(Student)
 admin.site.register(Course)
-admin.site.register(Cohort)
+# admin.site.register(Cohort)
 admin.site.register(Exam)
-admin.site.register(Coursework)
+admin.site.register(Assignment)
 admin.site.register(ClassHead)
 admin.site.register(Departments)
--- exam_board_preparation_app/models.py
+++ exam_board_preparation_app/models.py
@@ -1,47 +1,93 @@
+import datetime
 from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+######## Test Model #########
 class Departments(models.Model):
     id= models.AutoField(primary_key=True, unique=True)
     DepartmentName= models.CharField(max_length=255)
+#############################
 
-class Cohort(models.Model):
-    yearOfStudy = models.IntegerField(null=True)
-    degree = models.CharField(max_length=255) 
+# class Cohort(models.Model):
+#     requiredCredits = models.IntegerField()
+#     degreeTitle = models.CharField(max_length=100) 
+#     masters = models.BooleanField()
     
-    def __str__(self): return self.degree
+#     yearOfStudy_CHOICES = [
+#         (1, 'Level 1'),
+#         (2, 'Level 2'),
+#         (3, 'Level 3'),
+#         (4, 'Level 4'),
+#         (5, 'Level 5'),
+#         (6, 'Graduate'),
+#         (0, 'Other'),
+#     ]
+#     yearOfStudy = models.IntegerField(
+#         choices=yearOfStudy_CHOICES,
+#         default=0,
+#     )
+    
+#     def __str__(self): return ("Level " + self.yearOfStudy + " " + self.degree)
 
 class Student(models.Model):
-    metriculationNumber = models.CharField(max_length=10,null=True)
-    cohort = models.ForeignKey(Cohort, on_delete=models.CASCADE, null=True)
+    metriculationNumber = models.CharField(max_length=10)
+    name = models.CharField(max_length=225)
+    # cohort = models.ForeignKey(Cohort, on_delete=models.SET_NULL, null=True)
+    
+    ###### Info from Cohort ######
+    degreeTitle = models.CharField(max_length=100) 
+    mastersStudent = models.BooleanField()
+    fastRouteStudent = models.BooleanField()
+    
+    yearOfStudy_CHOICES = [
+        (1, 'Level 1'),
+        (2, 'Level 2'),
+        (3, 'Level 3'),
+        (4, 'Level 4'),
+        (5, 'Level 5'),
+        (6, 'Graduate'),
+        (0, 'Other'),
+    ]
+    yearOfStudy = models.IntegerField(
+        choices=yearOfStudy_CHOICES,
+        default=0,
+    )
+    #######################################
     
     def __str__(self): return self.metriculationNumber
 
-
 class Course(models.Model):
     classCode = models.CharField(max_length=255)
-    className = models.CharField(max_length=255, null=True)    
-    
+    className = models.CharField(max_length=255, null=True)  
+    credits = models.IntegerField()  
     students = models.ManyToManyField(Student)
+    year = models.CharField(max_length=9, default=(str(datetime.date.today().year) + "/" + str(datetime.date.today().year+1)))
     
-    def __str__(self): return self.classCode
+    def __str__(self): return (self.classCode + " - " + str(self.year))
     
-class Coursework(models.Model):
-    gradeMark = models.IntegerField()
+class Assignment(models.Model):
+    name = models.CharField(max_length=255)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    studnet = models.ForeignKey(Student, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    weighting = models.IntegerField()
+    gradeMark = models.IntegerField()
+    
     
 class Exam(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    weighting = models.IntegerField()
     q1Mark = models.IntegerField()
     q2Mark = models.IntegerField()
     q3Mark = models.IntegerField()
     
 class ClassHead(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    user = models.ManyToManyField(User)
     level = models.IntegerField()   
+    
+    def __str__(self): return ("Level " + str(self.level))
     
     
 
